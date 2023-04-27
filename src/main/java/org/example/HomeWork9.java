3ab2148dcfde8721b2355708f1732209d4baf784
Anton Fedoseev <fedoseev446@gmail.com>
2023-04-28 00:24:28 +0300

    Initial commit

--- src/main/java/org/example/HomeWork9.java
+++ src/main/java/org/example/HomeWork9.java
@@ -1,9 +1,14 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Scanner;
 
 public class HomeWork9 {
+    private static final Logger log = LoggerFactory.getLogger("HomeWork9");
     public static void main(String[] args) {
+        log.info("Программа запущена");
         do {
             Scanner scanner = new Scanner(System.in);
             System.out.print("Введите номер задачи (2-5): ");
